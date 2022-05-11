6b2bd4beac44b34cd22abdad69196dc01e93e726
Nick Pocock <pocockn@hotmail.co.uk>
2022-05-11 12:31:12 +0200

    Fix: Revert score from string to float64

--- model_identity.go
+++ model_identity.go
@@ -15,7 +15,7 @@ type Identity struct {
 	Status string         `json:"status,omitempty"`
 	Owner  *IdentityOwner `json:"owner,omitempty"`
 	// Float between 0 and 1 representing our confidence that this identity is valid. 0 meaning we couldn't verify any of the information provided with accuracy, and 1 absolute confidence.
-	Score string `json:"score,omitempty"`
+	Score float64 `json:"score,omitempty"`
 	// Unix-timestamp of when the identity was created
 	Created int64 `json:"created,omitempty"`
 	// Unix-timestamp of when the identity was updated
